74274f8c8d863a372e57d970e63d1db515112ada
Nirmal <nirmalkandel001@gmail.com>
2025-03-08 10:25:11 +0530

    profile view api calll

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils /constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils /userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <NavBar />
